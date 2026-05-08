5347d99f0913cabf7ab8ecb8158659c91dda2546
BokChoy45 <anguyencoco2003@gmail.com>
2026-05-08 02:06:53 -0500

    ok

--- Hw4.js
+++ Hw4.js
@@ -451,10 +451,10 @@ inputs.forEach(function (input) {
     });
 });
 
-var firstName = getCookie ("firstName");
+var firstName = getCookie("firstName");
 if (firstName !== "") {
     document.getElementById("welcome1").innerHTML = "Welcome back, " + firstName + "!<br>";
-    document.getElementById("welcome2").innerHTML = "<a href= '#' id = 'new-user'>NOT " 
+    document.getElementById("welcome2").innerHTML = "<a href= '#' id = 'new-user'>Not " 
     + firstName + "? Click here to start a new form.</a>";
 
     document.getElementById("new-user").addEventListener("click", function () {
